344d40a0de25c929cb3376cb08ca3c5a9d293b0f
Manish Kumar Sahu <manishkumar.sahu@adidas.com>
2025-01-24 13:27:21 +0530

    commit

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ model = joblib.load('model.joblib')
 
 @app.route('/')
 def home():
-    return render_template('./index.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     features = [float(x) for x in request.form.values()]
     prediction = model.predict([features])
-    return render_template('./index.html', prediction_text=f'Prediction: {prediction[0]}')
+    return render_template('index.html', prediction_text=f'Prediction: {prediction[0]}')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
